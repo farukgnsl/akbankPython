f17a4642183c0641b83203814fb4b1db6dad8a63
farukgnsl <87897509+farukgnsl@users.noreply.github.com>
2023-03-05 00:45:49 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import csv
+import datetime
+
+class Pizza:
+    def __init__(self):
+        def get_desciraption(self):
+            pass
+        def get_cost(self):
+            pass
+
+
+class Margarita(Pizza):
+    pass
+class Turkpizza(Pizza):
+    pass
+class PlainPizza(Pizza):
+    pass
+class Salami(Pizza):
+    pass
+
+class Italian(Pizza):
+    pass
+class Vegan(Pizza):
+    pass
+class Vegetarian(Pizza):
+    pass
+class Mix(Pizza):
+    pass
+
+class Decarator:
+    pass
+class Olives(Pizza):
+    pass
+class Mushrooms(Pizza):
+    pass
+
+class GoatCheese(Pizza):
+    pass
+class Meat(Pizza):
+    pass
+class Onions(Pizza):
+    pass
+class Corn(Pizza):
+    pass
+
+
+
+
